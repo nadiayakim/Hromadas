07161dcfe7cf136c74c047a8075240bdfaa89455
nadiayakim <58077823+nadiayakim@users.noreply.github.com>
2025-04-11 16:44:36 +0300

    Add files via upload

--- styles/Population2023_7_style.js
+++ styles/Population2023_7_style.js
@@ -20,7 +20,7 @@ var style_Population2023_7 = function(feature, resolution){
     if (feature.get("ADM3_EN") !== null) {
         labelText = String(feature.get("ADM3_EN"));
     }
-    if (value >= 3112.000000 && value <= 4544.400000) {
+    if (value >= 3112.000000 && value <= 5000.000000) {
             style = [ new ol.style.Style({
         image: new ol.style.Circle({radius: 8.0 + size,
             stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}), fill: new ol.style.Fill({color: 'rgba(18,113,88,1.0)'})}),
@@ -28,7 +28,7 @@ var style_Population2023_7 = function(feature, resolution){
                               labelFill, placement, bufferColor,
                               bufferWidth)
     })]
-                    } else if (value >= 4544.400000 && value <= 6019.000000) {
+                    } else if (value >= 5000.000000 && value <= 6000.000000) {
             style = [ new ol.style.Style({
         image: new ol.style.Circle({radius: 13.5 + size,
             stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}), fill: new ol.style.Fill({color: 'rgba(18,113,88,1.0)'})}),
@@ -36,7 +36,7 @@ var style_Population2023_7 = function(feature, resolution){
                               labelFill, placement, bufferColor,
                               bufferWidth)
     })]
-                    } else if (value >= 6019.000000 && value <= 12785.000000) {
+                    } else if (value >= 6000.000000 && value <= 12500.000000) {
             style = [ new ol.style.Style({
         image: new ol.style.Circle({radius: 19.0 + size,
             stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}), fill: new ol.style.Fill({color: 'rgba(18,113,88,1.0)'})}),
@@ -44,7 +44,7 @@ var style_Population2023_7 = function(feature, resolution){
                               labelFill, placement, bufferColor,
                               bufferWidth)
     })]
-                    } else if (value >= 12785.000000 && value <= 18948.600000) {
+                    } else if (value >= 12500.000000 && value <= 19000.000000) {
             style = [ new ol.style.Style({
         image: new ol.style.Circle({radius: 24.5 + size,
             stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}), fill: new ol.style.Fill({color: 'rgba(18,113,88,1.0)'})}),
@@ -52,7 +52,7 @@ var style_Population2023_7 = function(feature, resolution){
                               labelFill, placement, bufferColor,
                               bufferWidth)
     })]
-                    } else if (value >= 18948.600000 && value <= 27427.000000) {
+                    } else if (value >= 19000.000000 && value <= 27500.000000) {
             style = [ new ol.style.Style({
         image: new ol.style.Circle({radius: 30.0 + size,
             stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}), fill: new ol.style.Fill({color: 'rgba(18,113,88,1.0)'})}),
